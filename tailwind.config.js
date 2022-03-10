a73a694f69fea44fbd0f51fda90e0e810cfb1c04
pavan <pavangurram56@gmail.com>
2022-03-10 14:57:39 +0530

    css rules added to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
+      },
+      black: {
+        light: "#005c98",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
+    },
     extend: {},
   },
   plugins: [],
